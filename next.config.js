4464d1031efb5999a02b94c7d04a23ce47804d0c
Kevin Thomas <kevin.jt2007@gmail.com>
2025-03-14 17:37:15 -0700

    docs: add handbook

--- next.config.js
+++ next.config.js
@@ -11,6 +11,11 @@ module.exports = withMDX({
       source: '/',
       destination: 'https://scrapyard.hackclub.com/vancouver',
       permanent: true
-    }
+    },
+    {
+      source: '/handbook',
+      destination: 'https://kevinjosethomas.notion.site/Hacker-Handbook-Scrapyard-Vancouver-1b6b469a410d80b5abb9faa012d7f269',
+      permanent: true
+    },
   ]
 })
